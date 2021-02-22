51296681dce1f7f32c800e8ed563a8977f16bd39
Shiehand <andrewshieh7379@gmail.com>
2021-02-21 21:05:06 -0800

    added and tested auth

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,63 @@ import './App.css';
 import {PetsPage} from "./pages/pets";
 import {Route, Switch, BrowserRouter as Router} from "react-router-dom"
 
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { AmplifySignUp, AmplifyAuthenticator, AmplifySignOut }  from '@aws-amplify/ui-react';
+
+Amplify.configure(awsconfig);
+
 function App() {
     return (
-      <Router>
-        <TagNavbar />
-        <Switch>
-            <Route path="/pets">
-                <PetsPage />
-            </Route>
-            <Route path="/">
-                <HomePage />
-            </Route>
-        </Switch>
-      </Router>
+      <AmplifyAuthenticator usernameAlias="username">
+        <AmplifySignUp 
+          slot="sign-up"
+          usernameAlias="username"
+          formFields={[
+            {
+              type: "given_name",
+              label: "Enter your first name",
+              placeholder: "John",
+              required: true,
+            },
+            {
+              type: "family_name",
+              label: "Enter your last name",
+              placeholder: "Doe",
+              required: true,
+            },
+            {
+              type: "email",
+              label: "Enter your e-mail",
+              placeholder: "johndoe@gmail.com",
+              required: true,
+            },
+            {
+              type: "username",
+              label: "Enter your Username",
+              placeholder: "Username",
+              required: true,
+            },
+            {
+              type: "password",
+              label: "Enter your Password",
+              placeholder: "Password",
+              required: true,
+            },
+          ]} />
+        <Router>
+          <TagNavbar />
+          <AmplifySignOut />
+          <Switch>
+              <Route path="/pets">
+                  <PetsPage />
+              </Route>
+              <Route path="/">
+                  <HomePage />
+              </Route>
+          </Switch>
+        </Router>
+      </AmplifyAuthenticator>
   );
 }
 
