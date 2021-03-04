d714389f37110500c54306953d332756dc9abce4
VincentSastra <vincent.sastra@gmail.com>
2021-03-03 18:46:35 -0800

    Added some way to retrieve the API data

---
+++ src/api/api.ts
@@ -0,0 +1,19 @@
+import {Pet} from './pet'
+
+export async function getPets(username: string): Promise<Array<Pet>> {
+    return fetch("https://k7t0ap6b0i.execute-api.us-west-2.amazonaws.com/dev/users/shiehand/tags/")
+        .then(res =>
+            res.json()
+        )
+        .then(json => {
+            return json.result.Items.map(
+                (item: any) => {
+                    const pet: Pet = {
+                        name: item.SK.substr(4),
+                        img: item.Img
+                    }
+                    return pet
+                }
+            )
+        })
+}
