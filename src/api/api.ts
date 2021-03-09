572fb004e63390fb0ca55fd70b74a6e866431578
VincentSastra <vincent.sastra@gmail.com>
2021-03-09 12:59:48 -0800

    Change username to the values passed in

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import {Pet} from './pet'
 
 export async function getPets(username: string): Promise<Array<Pet>> {
-    return fetch("https://k7t0ap6b0i.execute-api.us-west-2.amazonaws.com/dev/users/shiehand/tags/")
+    return fetch("https://k7t0ap6b0i.execute-api.us-west-2.amazonaws.com/dev/users/" + username + "/tags/")
         .then(res =>
             res.json()
         )
