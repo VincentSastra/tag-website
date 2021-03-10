a72d8be63c89227424442bd68afae428455ddda6
VincentSastra <vincent.sastra@gmail.com>
2021-03-09 17:06:14 -0800

    Loads the pets correctly

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,24 +5,9 @@ import {MapWidget} from "../components/widget/Maps";
 import {Card, CardDeck, Col, Container, Row, Spinner} from "react-bootstrap";
 import {FlipCard} from "../components/widget/FlipCard";
 import {getPets} from "../api/api";
+import {Pet} from "../api/pet";
 
 const username = "Andrew"
-
-const petList = [
-    {
-        name: "Oreo",
-        img: "https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-    }, {
-        name: "Popi",
-        img: "https://dogtime.com/assets/uploads/2011/03/puppy-development-1280x720.jpg"
-    }, {
-        name: "Kucing",
-        img: "https://static.toiimg.com/photo/msid-67586673/67586673.jpg?3918697"
-    }, {
-        name: "Eva",
-        img: "https://img.huffingtonpost.com/asset/562e6c0f1400002b003c9220.jpeg?cache=yifce0exsu&ops=1778_1000"
-    }
-]
 
 function PetCard(name: string, img: string): JSX.Element {
     return FlipCard(
@@ -72,9 +57,15 @@ function PetCardList(petList: Array<{name: string, img: string}>): Array<JSX.Ele
 
 export function HomePage(): JSX.Element {
     const [loading, setLoading] = useState<boolean>(false)
+    const [petList, setPetList] = useState<Array<Pet>>([])
 
-    getPets("shiehand")
-        .then((res) => setLoading(true))
+    useEffect(() => {
+        getPets("shiehand")
+            .then((res) => {
+                setLoading(true)
+                setPetList(res)
+            })
+    }, []);
 
     return loading ? (
     <Container>
